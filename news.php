fbb9b83dcf267540e255c66edb623c56786dd7d6
Josh Bush <contact@jbushproductions.com>
2013-03-22 21:33:47 +1000

    Super mega extra dodgey news fixes. Dolphins are crying.

--- news.php
+++ news.php
@@ -44,14 +44,27 @@ $extraJS = "			getTwitters('tweet', {
 		{
 			if (!preg_match("/steamlug\/events\//", $item['link']))
 			{
-			$rssString .= "\t\t\t<article>\n";
-			$rssString .= "\t\t\t\t<div class = 'shadow'>\n";
-			$rssString .= "\t\t\t\t\t<h1><a href = '" . $item['link'] . "'>" . $item['title'] . "</a></h1>\n";
-			$rssString .= "\t\t\t\t\t<p class = 'attribution'>By " . $item['author'] . " on " . $item['pubDate'] . "</p>\n";
-			$rssString .= "\t\t\t\t\t<p>" . $item['description'] . "</p>\n";
-			$rssString .= "\t\t\t\t\t<p class = 'announcementlink'><a href = '" . $item['link'] . "'>View and comment on Steamcommunity.com</a></p>\n";
-			$rssString .= "\t\t\t\t</div>\n";
-			$rssString .= "\t\t\t</article>\n";
+			
+				$item['description'] = str_replace(" onclick=\"return AlertNonSteamSite( this );\"", "", $item['description']);
+				$item['description'] = str_replace(" class=\"bb_link\"", "", $item['description']);
+				$item['description'] = str_replace(" class=\"bb_ul\"", "", $item['description']);
+				$item['description'] = str_replace("<br><", "<", $item['description']);
+				$item['description'] = str_replace("<i>", "<em>", $item['description']);
+				$item['description'] = str_replace("</i>", "</em>", $item['description']);
+				$item['description'] = str_replace("<b>", "<strong>", $item['description']);
+				$item['description'] = str_replace("</b>", "</strong>", $item['description']);
+				$item['description'] = str_replace("<br>\n<br>", "</p><p>", $item['description']);
+				$item['description'] = str_replace("</ul>\n\n<br>", "</ul>\n<p>", $item['description']);
+				$item['description'] = str_replace("<ul>", "</p>\n<ul>", $item['description']);
+
+				$rssString .= "\t\t\t<article>\n";
+				$rssString .= "\t\t\t\t<div class = 'shadow'>\n";
+				$rssString .= "\t\t\t\t\t<h1><a href = '" . $item['link'] . "'>" . $item['title'] . "</a></h1>\n";
+				$rssString .= "\t\t\t\t\t<p class = 'attribution'>By " . $item['author'] . " on " . $item['pubDate'] . "</p>\n";
+				$rssString .= "\t\t\t\t\t<p>" . $item['description'] . "</p>\n";
+				$rssString .= "\t\t\t\t\t<p class = 'announcementlink'><a href = '" . $item['link'] . "'>View and comment on Steamcommunity.com</a></p>\n";
+				$rssString .= "\t\t\t\t</div>\n";
+				$rssString .= "\t\t\t</article>\n";
 			}
 		}
     	}
