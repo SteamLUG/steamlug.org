635fe16867abc789ac23b2312b8012d15626eaa1
Josh Bush <contact@jbushproductions.com>
2013-03-22 23:40:12 +1000

    Added placeholder text for projects page

--- projects.php
+++ projects.php
@@ -11,12 +11,11 @@ $pageTitle = "About";
 		<section>
 			<article>
 				<div class = 'shadow'>
-				<h1>Watch this space!</h1>
-				<p>Nunc in mollis urna. Donec sit amet sapien eget lectus lobortis mattis ut eget ligula. Nam feugiat diam ac lorem bibendum malesuada non sit amet augue. Etiam eget massa turpis. Fusce ornare risus a diam commodo vitae ultricies elit suscipit. Vestibulum dolor augue, eleifend a accumsan id, lacinia at diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<h1>Watch this spaaaaaaaaaaaaaace!</h1>
+				<p>We currently don't have anything up in this space, but there will be things soon!</p>
 
-<p>Praesent orci ipsum, convallis at molestie in, tempor quis nunc. Ut ultricies bibendum turpis, eget pulvinar lectus placerat eget. Sed facilisis lobortis placerat. Morbi volutpat metus sed velit placerat vel accumsan erat porta. Vestibulum quis eros nunc. Vestibulum felis orci, commodo in pulvinar sollicitudin, rutrum eget tortor. Ut mauris lorem, pellentesque non tempor at, sagittis nec mauris.</p>
+				<p>If you have a community project that you think belongs here, get in touch with <a href = 'http://steamcommunity.com/id/cheeseness'>Cheeseness</a>. If you need to catch him, he's often hanging out in the <a href = 'irc'>#SteamLUG IRC channel</a>.</p>
 
-<p>Phasellus a dolor tortor. Curabitur sollicitudin pretium nisi, non vulputate lorem congue in. Nulla in lectus at orci malesuada iaculis. Fusce tristique eros a ipsum rutrum ut consequat elit iaculis. Pellentesque elit turpis, lobortis sed consequat nec, scelerisque eget quam. Morbi et nibh dui, at venenatis tellus. Nunc tempor dolor sit amet dolor hendrerit malesuada. Suspendisse ac massa elit, fringilla ornare eros.</p>
 				</div>
 			</article>
 		</section>
