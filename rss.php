1086f958f9008889c65cc0377cfca44ba556102f
Josh Bush <contact@jbushproductions.com>
2013-03-23 11:59:33 +1000

    Added initial events RSS feed (trying using the events parser)

--- rss.php
+++ rss.php
@@ -3,7 +3,6 @@
 	header("Content-Type: application/rss+xml");
 	header("Access-Control-Allow-Origin: *");
 
-	include_once("includes/lastRSS.php");
 
 	echo "<?xml version='1.0'?>\n";
 	echo "<rss version='2.0' xmlns:atom='http://www.w3.org/2005/Atom'>\n";
@@ -33,45 +32,59 @@
 	echo "\t<pubDate>";
 	echo date("r");
 	echo "</pubDate>\n";
-	$rss = new lastRSS;
-	$rss->cache_dir = './temp';
-	$rss->cache_time = 1200;
-	$rss->CDATA = 'content';
-	$rss->items_limit = 20;
-	$rssString = "";
-	if ($rs = $rss->get('http://steamcommunity.com/groups/steamlug/rss'))
-	{
-		foreach($rs['items'] as $item)
-		{
-			if (preg_match("/steamlug\/events\//", $item['link']))
-			{
-				$item['description'] = str_replace(array("\r", "\r\n"), "\n", $item['description']);
-				$item['description'] = str_replace(" onclick=\"return AlertNonSteamSite( this );\"", "", $item['description']);
-				$item['description'] = str_replace(" class=\"bb_link\"", "", $item['description']);
-				$item['description'] = str_replace(" class=\"bb_ul\"", "", $item['description']);
-				$item['description'] = str_replace("<br><", "<", $item['description']);
-				$item['description'] = str_replace("<i>", "<em>", $item['description']);
-				$item['description'] = str_replace("</i>", "</em>", $item['description']);
-				$item['description'] = str_replace("<b>", "<strong>", $item['description']);
-				$item['description'] = str_replace("</b>", "</strong>", $item['description']);
-				$item['description'] = str_replace("<br>-----", "-----", $item['description']);
-				$item['description'] = str_replace("<br>\n<br>", "</p><p>", $item['description']);
-				$item['description'] = str_replace("</ul>\n\n<br>", "</ul>\n<p>", $item['description']);
-				$item['description'] = str_replace("<ul>", "</p>\n<ul>", $item['description']);
-				$item['description'] = str_replace("<br>", "<br />", $item['description']);
 
-				echo "<item>\n";
-					echo "<title>" . $item['title'] . "</title>\n";
-					echo "<link>" . $item['link'] . "</link>\n";
-					echo "<description>" . $item['description'] . "</description>\n";
-					echo "<author>steamlug@gmail.com (SteamLUG)</author>\n";
-					echo "<pubDate>" .  $item['pubDate'] ."</pubDate>\n";
-					echo "<guid>" . $item['link'] . "</guid>\n";
-					echo "<category>Event</category>\n";
-				echo "</item>\n";
-			
+
+	$parser = new SteamEventParser();
+	$data = $parser->genData("steamlug");
+	$d = explode("-", $data['events'][0]['date']);
+	$t = explode(":", $data['events'][0]['time']);
+	$dateString = "var target = Math.round( Date.UTC (" . $d[0] . ", " . $d[1] . " -1, " . $d[2] . ", " . $t[0] . ", " . $t[1] . ", 0, 0) / 1000);";
+
+	$month = gmstrftime("%m");
+	$year = gmstrftime("%Y");
+	$data2 = $parser->genData("steamlug", $month >= 12 ? 1: $month +1, $month >= 12 ? $year + 1: $year);
+
+	$data['events'] = array_merge($data['events'], $data2['events']);
+	$timezone = new DateTimeZone('UTC'); 
+	$nowDate = new DateTime("now", $timezone);
+	foreach($data['events'] as $event)
+	{
+
+		//TODO: We probably should be using whatever timezone the events were using to begin with
+		$tempDate = new DateTime($event['date'] . " " . $event['time'], $timezone);
+		$timeLeft = "Under an hour";
+		if ($tempDate->sub(new TimeInterval("P1H")) > $nowDate)
+		{
+			$timeLeft = "Under 24 hours";
+			if ($tempDate->sub(new TimeInterval("P23H")) > $nowDate)
+			{
+				$timeLeft = "Under 1 week";
+				if ($tempDate->sub(new TimeInterval("P6D")) > $nowDate)
+				{
+					$timeLeft = "Ages ;_;";
+					continue;			
+				}
 			}
 		}
+	
+		echo "<item>\n";
+			echo "<title>" . $event['title'] . "</title>\n";
+			echo "<link>" . $event['url'] . "</link>\n";
+			echo "<description>" . $event['title'] . " at " . $event['date'] . " " . $event['time'] . " " . $event['tz'] . " (" . $timeUeft. " away)</description>\n";
+			echo "<author>steamlug@gmail.com (SteamLUG)</author>\n";
+			echo "<pubDate>". $tempDate->format("RSS") . "</pubDate>\n";
+			echo "<guid>" . $event['url'] . "</guid>\n";
+			echo "<category>Event</category>\n";
+		echo "</item>\n";
+		
+		
+				$eventString = "\t\t\t<li>\n";
+
+		$eventString .= "\t\t\t\t<img class = 'eventLogo' src = '" . $event["img_small"] . "' alt = " . $event["title"] . ">\n";
+		$eventString .= "\t\t\t\t<a class = 'eventName' href = '" . $event["url"] . "'>" . $event["title"] . "</a><span class = 'eventDate'>" . $event['date'] . " " . $event['time'] . " " . $event['tz'] . "</span>\n";
+
+		$eventString .= "\t\t\t</li>\n";
+		echo $eventString;
 	}
 	
 	echo "</channel>";
