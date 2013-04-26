1d693994175508a63233e9f7ac4965dd01943baa
Josh Bush <contact@jbushproductions.com>
2013-04-26 19:30:52 +1000

    Trying out something to fix current month's events

--- feed/events.json.php
+++ feed/events.json.php
@@ -3,12 +3,12 @@ require_once("../rbt_prs.php");
 require_once("../steameventparser.php");
 $parser = new SteamEventParser();
 $data = $parser->genData("steamlug");
-$month = gmstrftime("%m");
+$month = gmstrftime("%m")-0; // Yuck, apparently the 0 breaks something?
 $year = gmstrftime("%Y");
 $data = $parser->genData("steamlug", $month, $year);
 
-$data2 = $parser->genData("steamlug", $month >= 12 ? 1: $month +1, $month >= 12 ? $year + 1: $year);
-$data3 = $parser->genData("steamlug", $month <= 1 ? 12: $month -1, $month <= 1 ? $year -1: $year);
+$data2 = $parser->genData("steamlug", ( $month >= 12 ? 1 : ( $month +1 ) ), ( $month >= 12 ? ( $year + 1 ) : $year ));
+$data3 = $parser->genData("steamlug", ( $month <= 1 ? 12 : ( $month -1 ) ), ( $month <= 1 ? ( $year -1 ) : $year ));
 
 $data['events'] = array_merge($data['events'], $data2['events']);
 $data['pastevents'] = array_merge($data['pastevents'], $data3['pastevents']);
