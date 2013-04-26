de5d42d736b209e174330030ee9744f01e7a75ac
Josh Bush <contact@jbushproductions.com>
2013-04-26 19:12:23 +1000

    Put detail link into a H3 and added styling

--- events.php
+++ events.php
@@ -36,7 +36,7 @@ $externalJS = array('scripts/events.js');
 	$eventString = "\t\t\t\t<h2><a href='" . $data["events"][0]["url"] . "'>" .  $data["events"][0]["title"] . "</a></h2>";
 	$eventString .= "\t\t\t\t\t<img src='http://cdn.steampowered.com/v/gfx/apps/" . $data["events"][0]["appid"] . "/header.jpg' alt='" . $data["events"][0]["title"] . "'/>\n";
 	$eventString .= "\t\t\t\t</a>\n";
-	$eventString .= "\t\t\t\t<a href='" . $data["events"][0]["url"] . "'>Click for details</a>\n";
+	$eventString .= "\t\t\t\t<h3 class = 'detailLink'><a href='" . $data["events"][0]["url"] . "'>Click for details</a></h3>\n";
 	echo $eventString;
 ?>
 				<div id="countdown">
