aa2189ab659129464c15ab2f275393aa3133d264
Mickey William Fischer <mwf@swordfischer.com>
2013-05-03 09:49:36 +0200

    And added giveaway.php file

---
+++ giveaway.php
@@ -0,0 +1,36 @@
+<?php include_once("/var/www/steamlug.org/includes/header.php"); ?>
+        <header>
+            <hgroup>
+                <h1>Giveaway</h1>
+            </hgroup>
+        </header>
+<section>
+        <article id='about'>
+            <div class="shadow">
+                <h1>About</h1>
+                <p>Welcome to the SteamLUG giveaway page!</p>
+                <p>If you managed to get to this super secret giveaway page, it must mean that you are a very special person!</p>                <p>Below, you will find 3 (three) keys cut up into 9 (nine) pieces, mix and match these to unlock a key for</p>
+                 <ul>
+                    <li>2 (two) <a href="http://store.steampowered.com/app/1200/">Red Orchestra: Ostfront 41-45</a></li>
+                    <li>2 (two) <a href="http://store.steampowered.com/app/65800/">Dungeon Defenders</a> <i>(includes all DLC)</i></li>
+                </ul>
+                <p>If you unlock a key, please let everyone know in the thread!</p>
+
+                <h1>The Puzzle</h1>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.35.47_n58cdcjjri.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.03_ure1zrjtg2.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.21_oepv8wm6nu.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.32_jc66veahyu.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.39_y6jt79owdp.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.47_dfcz7o5z0y.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.36.56_lxnd1m3gzo.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.37.03_ytegkdaoms.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.37.16_mloml4qvmh.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.37.22_x4h42boxyn.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.37.28_zzxd4y7315.png)" class="key"></div>
+<div style="background-image: url(https://hakd.org/~swordfischer/dump/2013-05-02_14.37.32_odr1j02aqk.png)" class="key"></div>
+            <br class="clearfix">
+            </div>
+        </article>
+        </section>
+<?php include_once("/var/www/steamlug.org/includes/footer.php"); ?> 
