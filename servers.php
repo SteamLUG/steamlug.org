8a2ae7e4095261a1562f1e7c969de51842be958d
Mickey William Fischer <mwf@swordfischer.com>
2013-05-24 11:19:56 +0200

    Changed server information

--- servers.php
+++ servers.php
@@ -25,13 +25,17 @@ $pageTitle = "Servers";
 define( 'SQ_TIMEOUT', 1 );
 define( 'SQ_ENGINE', SourceQuery :: SOURCE );
 
-$ServerHost = "dannebrog.steamlug.org";
-$Ports = file( "/var/www/cenobite.swordfischer.com/servers.txt");
+$Servers = file( "/var/www/cenobite.swordfischer.com/servers2.txt" );
+
+foreach ( $Servers as $Server )
+	{
+		list ( $ServerHost[], $Ports[] ) = preg_split ( "/:/", $Server );
+	}
 
 $Query = new SourceQuery( );
-foreach ($Ports as $Port)
+foreach ( $ServerHost as $Index => $Host)
 	{
-		$Query->Connect( $ServerHost, $Port, SQ_TIMEOUT, SQ_ENGINE );
+		$Query->Connect( $Host, $Ports[$Index], SQ_TIMEOUT, SQ_ENGINE );
 		$Info = $Query->GetInfo( );
 
 		$serverString  = "\t<article>\n";
@@ -44,7 +48,7 @@ foreach ($Ports as $Port)
 		$serverString .= "\t\t\t<img class='serverimg' src='images/server_offline.png' alt = 'Offline server' />\n";
 		$serverString .= "\t\t\t<dl>\n";
 		$serverString .= "\t\t\t<dt>Map</dt><dd>Unknown</dd>\n";
-		$serverString .= "\t\t\t<dt>Host</dt><dd>" . $ServerHost . "</dd>\n";
+		$serverString .= "\t\t\t<dt>Host</dt><dd>" . $Host . "</dd>\n";
 		$serverString .= "\t\t\t<dt>Port</dt><dd>Unknown</dd>\n";
 		$serverString .= "\t\t\t<dt>Players</dt><dd>Unknown</dd>\n";
 		$serverString .= "\t\t\t<dt>Max Players</dt><dd>Unknown</dd>\n";
@@ -56,11 +60,11 @@ foreach ($Ports as $Port)
 		}
 		else
 		{
-		$serverString .= "\t\t\t<h1><a href='steam://connect/" . $ServerHost . ":" . $Info["GamePort"] . "'>" . $Info["HostName"] . "</a></h1>\n";
-		$serverString .= "\t\t\t<a href='steam://connect/" . $ServerHost . ":" . $Info["GamePort"] . "'><img class='serverimg' src='http://cdn.steampowered.com/v/gfx/apps/" . $Info["AppID"] . "/header.jpg' alt = 'Game logo' /></a>\n";
+		$serverString .= "\t\t\t<h1><a href='steam://connect/" . $Host . ":" . $Info["GamePort"] . "'>" . $Info["HostName"] . "</a></h1>\n";
+		$serverString .= "\t\t\t<a href='steam://connect/" . $Host . ":" . $Info["GamePort"] . "'><img class='serverimg' src='http://cdn.steampowered.com/v/gfx/apps/" . $Info["AppID"] . "/header.jpg' alt = 'Game logo' /></a>\n";
 		$serverString .= "\t\t\t<dl>\n";
 		$serverString .= "\t\t\t<dt>Map</dt><dd>" . $Info["Map"] . "</dd>\n";
-		$serverString .= "\t\t\t<dt>Host</dt><dd>" . $ServerHost . "</dd>\n";
+		$serverString .= "\t\t\t<dt>Host</dt><dd>" . $Host . "</dd>\n";
 		$serverString .= "\t\t\t<dt>Port</dt><dd>" . $Info["GamePort"] . "</dd>\n";
 		$serverString .= "\t\t\t<dt>Players</dt><dd>" . $Info["Players"] . "</dd>\n";
 		$serverString .= "\t\t\t<dt>Max Players</dt><dd>" . $Info["MaxPlayers"] . "</dd>\n";
@@ -68,7 +72,7 @@ foreach ($Ports as $Port)
 		$serverString .= "\t\t\t<dt>Secure</dt><dd>" . $Info["Secure"] . "</dd>\n";
 		$serverString .= "\t\t\t<dt>Version</dt><dd>" . $Info["Version"] . "</dd>\n";
 		$serverString .= "\t\t\t</dl>\n";
-		$serverString .= "\t\t\t<p class = 'serverlink'><a href='steam://connect/" . $ServerHost . ":" . $Info["GamePort"] . "'>Click here to join</a></p>\n";
+		$serverString .= "\t\t\t<p class = 'serverlink'><a href='steam://connect/" . $Host . ":" . $Info["GamePort"] . "'>Click here to join</a></p>\n";
 		}
 		$serverString .= "\t\t</div>\n";
 		$serverString .= "\t</article>\n";
