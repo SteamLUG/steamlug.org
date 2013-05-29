9473e71263cb60b827c7219ef7071850b74d86d0
Mickey William Fischer <mwf@swordfischer.com>
2013-05-29 20:49:23 +0200

    Higher width for the audiotag, and rearranged the flac and ogg listing

--- cast.php
+++ cast.php
@@ -77,11 +77,11 @@
                 $listItem .= "<h3>" . str_replace('-', ' ', $matches[3]) . "</h3>";
                 $listItem .= "<p>Season: $matches[1], Episode: $matches[2]</p>";
                 $listItem .= "<audio preload='none' src='$url$file.ogg' type='audio/ogg' controls>Your browser does not support the &ltaudio&gt tag.</audio>";
-                $listItem .= "<span class='right'>";
-                $listItem .= round(filesize($filename)/1024/1024,2) . " MB <a href='$url$file.flac'>FLAC</a> | ";
+                $listItem .= "<p>";
                 $listItem .= round(filesize(str_replace('flac', 'ogg', $filename))/1024/1024,2) . " MB <a href='$url$file.ogg'>OGG</a> | ";
+                $listItem .= round(filesize($filename)/1024/1024,2) . " MB <a href='$url$file.flac'>FLAC</a> | ";
                 $listItem .= round(filesize(str_replace('flac', 'mp3', $filename))/1024/1024,2) . " MB <a href='$url$file.mp3'>MP3</a> ";
-                $listItem .= "</span>";
+                $listItem .= "</p>";
                 $listItem .= "</li>";
                 echo $listItem;
             } 
