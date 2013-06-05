087394e623c126a4e0d0e452a3d93968c34cdae4
Cheeseness (Josh Bush) <contact@jbushproductions.com>
2013-06-05 18:16:52 +1000

    Forced nav links to be absolute (quickfix for cast archive pages)

--- includes/header.php
+++ includes/header.php
@@ -136,27 +136,27 @@ if (!isset($pageTitle))
 ?>
 		<nav>
 			<ul>
-				<li class = '<?php echo $newsPage; ?>'><a href = 'news'>News</a></li>
-				<li class = '<?php echo $chatPage; ?>'><a href = 'irc'>Chat</a>
+				<li class = '<?php echo $newsPage; ?>'><a href = '/news'>News</a></li>
+				<li class = '<?php echo $chatPage; ?>'><a href = '/irc'>Chat</a>
 					<ul class = '<?php echo $chatPage; ?>'>
-						<li class = '<?php echo $ircPage; ?>'><a href = 'irc'>IRC (text)</a></li>
-						<li class = '<?php echo $mumblePage; ?>'><a href = 'mumble'>Mumble (voice)</a></li>
+						<li class = '<?php echo $ircPage; ?>'><a href = '/irc'>IRC (text)</a></li>
+						<li class = '<?php echo $mumblePage; ?>'><a href = '/mumble'>Mumble (voice)</a></li>
 					</ul>
 				</li>
 				<li class = '<?php echo $groupPage; ?>'><a href = 'http://steamcommunity.com/groups/steamlug/'>Group</a></li>
-				<li class = '<?php echo $gamingPage; ?>'><a href = 'events'>Gaming</a>
+				<li class = '<?php echo $gamingPage; ?>'><a href = '/events'>Gaming</a>
 					<ul>
-						<li class = '<?php echo $eventsPage; ?>'><a href = 'events'>Events</a></li>
-						<li class = '<?php echo $serversPage; ?>'><a href = 'servers'>Servers</a></li>
-						<li class = '<?php echo $streamPage; ?>'><a href = 'stream'>Live Stream</a></li>
+						<li class = '<?php echo $eventsPage; ?>'><a href = '/events'>Events</a></li>
+						<li class = '<?php echo $serversPage; ?>'><a href = '/servers'>Servers</a></li>
+						<li class = '<?php echo $streamPage; ?>'><a href = '/stream'>Live Stream</a></li>
 					</ul>
 				</li>
-				<li class = '<?php echo $projectsPage; ?>'><a href = 'projects'>Projects</a>
+				<li class = '<?php echo $projectsPage; ?>'><a href = '/projects'>Projects</a>
 					<ul>
-						<li class = '<?php echo $overviewPage; ?>'><a href = 'projects'>Overview</a></li>
-						<li class = '<?php echo $castPage; ?>'><a href = 'cast'>SteamLUG Cast</a></li>
+						<li class = '<?php echo $overviewPage; ?>'><a href = '/projects'>Overview</a></li>
+						<li class = '<?php echo $castPage; ?>'><a href = '/cast'>SteamLUG Cast</a></li>
 					</ul>
 				</li>
-				<li class = '<?php echo $aboutPage; ?>'><a href = 'about'>About</a></li>
+				<li class = '<?php echo $aboutPage; ?>'><a href = '/about'>About</a></li>
 			</ul>
 		</nav>
