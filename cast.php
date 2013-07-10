c4ac3323845e99dd27a202ba717332cb1a76414c
Mickey William Fischer <mwf@swordfischer.com>
2013-07-10 22:28:13 +0200

    Maybe this

--- cast.php
+++ cast.php
@@ -21,11 +21,11 @@ if (!function_exists('glob_recursive'))
 	function glob_recursive($pattern, $flags = 0)
 	{
 		$files = glob($pattern, $flags);
-		foreach (rsort(glob(dirname($pattern).'/*', GLOB_ONLYDIR)) as $dir)
+		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR) as $dir)
 		{
 			$files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
 		}
-	return $files;
+	return rsort($files);
 	}
 }
 include_once('includes/header.php');
