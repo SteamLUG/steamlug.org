81a5a2ab6dfacf27d58b441ba66679d45b28ded2
Mickey William Fischer <mwf@swordfischer.com>
2013-07-10 22:47:01 +0200

    YES.. Maybe?

--- cast.php
+++ cast.php
@@ -21,7 +21,7 @@ if (!function_exists('glob_recursive'))
 	function glob_recursive($pattern, $flags = 0)
 	{
 		$files = glob($pattern, $flags);
-		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR) as $dir)
+		foreach (array_reverse(glob(dirname($pattern).'/*', GLOB_ONLYDIR)) as $dir)
 		{
 			$files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
 		}
@@ -29,7 +29,6 @@ if (!function_exists('glob_recursive'))
 	}
 }
 include_once('includes/header.php');
-var_dump(glob_recursive());
 ?>
 	<header>
 		<h1>SteamLUG Cast</h1>
