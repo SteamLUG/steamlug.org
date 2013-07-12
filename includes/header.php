d052f5adbeafb160a1b30a2484ef976d458b5353
Cheeseness (Josh Bush) <contact@jbushproductions.com>
2013-07-12 15:10:15 +1000

    Added RSS link tag to header include. Should make it easier for people with RSS readers that auto-detect feeds for sites.

--- includes/header.php
+++ includes/header.php
@@ -22,6 +22,7 @@ if (!isset($pageTitle))
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name = 'description' content = "<?php echo $description; ?>" />
 		<meta name = 'keywords' content = "<?php echo $keywords; ?>" />
+		<link rel="alternate" type="application/rss+xml" title="RSS" href="http://steamcommunity.com/groups/steamlug/rss/" />
 		<title>SteamLUG <?php echo $pageTitle; ?></title>
 		<link rel="stylesheet" href="/css/style.css" type="text/css" />
 		<script>
