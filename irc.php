537870cd563453c9807c44b3dce128eaf59b121d
Mickey William Fischer <mwf@swordfischer.com>
2013-09-26 18:19:34 +0200

    last change

--- irc.php
+++ irc.php
@@ -20,7 +20,7 @@ $pageTitle = "Chat";
 			</article>
 			<article>
 				<div class = 'shadow'>
-				<iframe src="http://webchat.freenode.net?channels=steamlug" width = "640" height = "600">
+				<iframe src="https://webchat.freenode.net?channels=steamlug" width = "640" height = "600">
 					<p>Your browser does not support iframes.</p>
 				</iframe>
 				</div>
