a2f76be9134894fc8d0b9d43ac016d5890d06524
John Drinkwater <john@nextraweb.com>
2014-06-13 12:59:41 +0100

    Bump ValiantCheese to gravatar as well

--- cast.php
+++ cast.php
@@ -46,7 +46,7 @@ function slenc($u)
 /* TODO: join this to our steamlug user system; TODO: make steamlug user system */
 $hostAvatars = array(
 		"swordfischer" =>	"https://pbs.twimg.com/profile_images/3091650213/abd95819b5fa2ac94d26866446404b65.png",
-		"ValiantCheese" =>	"https://pbs.twimg.com/profile_images/378800000805813456/cecaa38c6d23004e4ca53c85773b456d.png",
+		"ValiantCheese" =>	"http://gravatar.com/avatar/916ffbb1cd00d10f5de27ef4f9846390",
 		"johndrinkwater" =>	"http://gravatar.com/avatar/751a360841982f0d0418d6d81b4beb6d",
 		"MimLofBees" =>		"https://pbs.twimg.com/profile_images/2458841225/cnm856lvnaz4hhkgz6yg.jpeg",
 		"DerRidda" =>		"https://pbs.twimg.com/profile_images/2150739768/pigava.jpeg",
