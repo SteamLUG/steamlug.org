91e1ce1860fca8fd417da511fde2562d02354761
Andreas Struller <andreas@potyomkin.org>
2015-02-21 21:13:28 +0100

    New protocols for GameQ.

---
+++ includes/gameq/protocols/dst.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Don't Starve Together Protocol Class
+ *
+ * Note that the game port is the query port + 1
+ */
+class GameQ_Protocols_Dst extends GameQ_Protocols_Source
+{
+	protected $name = "dst";
+	protected $name_long = "Don't Starve Together";
+}
--- includes/gameq/protocols/rwr.php
+++ includes/gameq/protocols/rwr.php
@@ -1,6 +1,10 @@
 <?php
-
-class GameQ_Protocols_Rwr extends GameQ_Protocols_Quake3
+/**
+ * Running with Rifles Protocol Class
+ *
+ * Note that the query port is the server connect port + 1
+ */
+class GameQ_Protocols_Rwr extends GameQ_Protocols_Source
 {
 	protected $name = "rwr";
 	protected $name_long = "Running with Rifles";
