db285eb17c827fb697c41e50a2ae57ed4b150e87
John Drinkwater <john@nextraweb.com>
2015-03-18 16:48:05 +0000

    Session tweaks
    Reasons for these changes:
    * move more defaults to PHP settings
    * according to PHP docs, we need to set cookie_httponly for each script…
    * avoid session_name(), use default, set via ini_set instead
    * session_write_close() because apparently it speeds up page?
        yet it isn’t clear to me if this is a real close()? or just to
        prevent writing. Docs are bad™
    * we didn’t appear to be doing the right thing in logout()
    * setting session_name after cookie params was wrong, as it might set on
        PHPSESSID instead.

--- includes/session.php
+++ includes/session.php
@@ -1,4 +1,8 @@
 <?php
+	ini_set( 'session.use_only_cookies', 1 );
+	ini_set( 'session.use_strict_mode', 1 );
+	ini_set( 'session.name', 'steamlug' );
+	ini_set( 'session.cookie_httponly', 1 );
 
 	include_once('steam.php');
 	include_once('creds.php');
@@ -7,14 +11,10 @@
 
 		$currentSesh = session_id( );
 		if ( empty( $currentSesh ) ) {
-			$session_name = 'steamlug'; // Set a custom session name
 			$secure = false; // Set to true if using https.
 			$httponly = true; // This stops javascript being able to access the session id.
-
-			ini_set('session.use_only_cookies', 1); // Forces sessions to only use cookies.
 			$cookieParams = session_get_cookie_params(); // Gets current cookies params.
 			session_set_cookie_params($cookieParams["lifetime"], $cookieParams["path"], $cookieParams["domain"], $secure, $httponly);
-			session_name($session_name); // Sets the session name to the one set above.
 			session_start(); // Start the php session
 			session_regenerate_id(true); // regenerated the session, delete the old one
 		}
@@ -27,15 +27,12 @@
 		store_user_details($uid);
 		$_SESSION['i'] = getenv("REMOTE_ADDR");
 		$_SESSION['t'] = time() + (12 * 60 * 60);
+		session_write_close();
 	}
 
 	function logout()
 	{
-		echo "Logging out";
 		SteamSignIn::logout();
-		$_SESSION = array();
-		$cookieParams = session_get_cookie_params();
-		// setcookie(session_name(),  '', time() - 42000, $cookieParams["path"], $cookieParams["domain"], $cookieParams["secure"], $cookieParams["httponly"]);
 		session_destroy();
 		header ("Location: /");
 	}
@@ -52,10 +49,13 @@
 				{
 					$checkResult = true;
 					$_SESSION['t'] = $t + (12 * 60 * 60);
+
 				}
 			}
+			// we should never write to session again in this process
+			// but does this mean we can never update the time again?
+			session_write_close();
 		}
-		//session_write_close();
 		return $checkResult;
 	}
 
