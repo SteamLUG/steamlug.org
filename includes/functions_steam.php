ecb2c047ef11559da0c3329b74412d5243b7b388
John Drinkwater <john@nextraweb.com>
2015-03-19 17:07:49 +0000

    Add utility function for workshop items
    Initial work for #63. This should probably return HTML of a similar
    design to Steam’s appearance, rather than returning JSON.

--- includes/functions_steam.php
+++ includes/functions_steam.php
@@ -5,9 +5,24 @@
 
 	function getAdminNames() {
 		$admins = implode(",", getAdmins());
+		// TODO remember to curl this!
 		$reply = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . getSteamAPIKey() . '&steamids=' . $admins);
 		$users = json_decode($reply, true);
 		$users = $users["response"]["players"];
 		asort( $users );
 		return $users;
+	}
+
+	/*
+	 * Small utility function to return JSON details about a workshop item
+	 * http://steamcommunity.com/sharedfiles/filedetails/?id=[0-9]*
+	 * we are going to assume the call has parsed URL and stripped the id off for us
+	 */
+	function getWorkshopDetails( $id ) {
+
+		// TODO allow this to take array of ids in future, return array of details
+		// TODO remember to curl this!
+		$reply = file_get_contents('http://api.steampowered.com/service/PublishedFile/GetDetails/v1/?key=' . getSteamAPIKey() . '&publishedfileids[0]=' . $id );
+		$details = json_decode($reply, true);
+		return $details['response']['publishedfiledetails'];
 	}
