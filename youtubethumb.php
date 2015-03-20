SteamLUG/steamlug.org

commit a62be488413e506bd8e4969349460b81c84a7c47

Make youtube* scripts use functions_cast

youtubethumb.php
@@ -6,8 +6,8 @@ $season  = str_pad($season, 2, '0', STR_PAD_LEFT);
 $episode = isset($_GET["e"]) ? intval($_GET["e"]) : "0";
 $episode = str_pad($episode, 2, '0', STR_PAD_LEFT);
 
-include_once('includes/paths.php');
 include_once('includes/functions_avatars.php');
+include_once('includes/functions_cast.php');
 
 function nameplate( $string, $offset = 0, $guest = 0 ) {
 
@@ -68,21 +68,10 @@ $filename = $notesPath . "/s" . $season . "e" . $episode . "/episode.txt";
 /* User wanting to see a specific cast, and shownotes file exists */
 if ($season !== "00" && $episode !== "00" && file_exists($filename))
 {
-	$shownotes		= file($filename);
+	$shownotes			= file( $filename );
+	$meta				= castHeader( array_slice( $shownotes, 0, 14 ) );
 
-	$head = array_slice( $shownotes, 0, 14 );
-	$meta = array_fill_keys( array('RECORDED', 'PUBLISHED', 'TITLE',
-						'SEASON', 'EPISODE', 'DURATION', 'FILENAME',
-				'DESCRIPTION','HOSTS','GUESTS','ADDITIONAL', 'YOUTUBE' ), '');
-	foreach ( $head as $entry ) {
-		list($k, $v) = explode( ':', $entry, 2 );
-		$meta[$k] = trim($v);
-	}
-
-	$castHosts			= array_map('trim', explode(',', $meta['HOSTS']));
-	$castGuests			= array_map('trim', explode(',', $meta['GUESTS']));
 	$devGames			= array_map('trim', explode(',', $meta['ADDITIONAL']));
-
 	$listGames = [];
 
 	$guestsBlockOffset = 0; $hostsBlockOffset = 0;
@@ -90,16 +79,16 @@ if ($season !== "00" && $episode !== "00" && file_exists($filename))
 	$guestsIncludeString = ""; $hostsIncludeString = "";
 	$alignment = array(0, 610, 520, 430, 340, 250, 160, 50);
 
-	$hostsBlockOffset = $alignment[count($castHosts)]; $startIndex = 0;
-	foreach ($castHosts as $Host) {
+	$hostsBlockOffset = $alignment[count($meta['HOSTS'])]; $startIndex = 0;
+	foreach ($meta['HOSTS'] as $Host) {
 
 		if ($Host == "") break;
 		$hostsIncludeString .= nameplate( $Host, $startIndex ) ;
 		$startIndex += 180;
 	}
 
-	$guestsBlockOffset = $alignment[count($castGuests)]; $startIndex = 0;
-	foreach ($castGuests as $Guest) {
+	$guestsBlockOffset = $alignment[count($meta['GUESTS'])]; $startIndex = 0;
+	foreach ($meta['GUESTS'] as $Guest) {
 
 		if ($Guest == "") break;
 		$guestsIncludeString .= nameplate( $Guest, $startIndex, 1 );

youtubedescription.php
@@ -5,7 +5,7 @@ $season  = str_pad($season, 2, '0', STR_PAD_LEFT);
 $episode = isset($_GET["e"]) ? intval($_GET["e"]) : "0";
 $episode = str_pad($episode, 2, '0', STR_PAD_LEFT);
 
-include_once('includes/paths.php');
+include_once('includes/functions_cast.php');
 
 function slenc($u)
 {
@@ -15,19 +15,11 @@ function slenc($u)
 $filename = $notesPath . "/s" . $season . "e" . $episode . "/episode.txt";
 if ($season !== "00" && $episode !== "00" && file_exists($filename))
 {
-	$shownotes		= file($filename);
+	$shownotes			= file( $filename );
+	$meta				= castHeader( array_slice( $shownotes, 0, 14 ) );
 
-	$head = array_slice( $shownotes, 0, 14 );
-	$meta = array_fill_keys( array('RECORDED', 'PUBLISHED', 'TITLE',
-						'SEASON', 'EPISODE', 'DURATION', 'FILENAME',
-				'DESCRIPTION','HOSTS','GUESTS','ADDITIONAL', 'YOUTUBE' ), '');
-	foreach ( $head as $entry ) {
-		list($k, $v) = explode( ':', $entry, 2 );
-		$meta[$k] = trim($v);
-	}
-	$epi = 's' . slenc($meta['SEASON']) . 'e' . slenc($meta['EPISODE']);
-
-	$meta['RECORDED']  = ( $meta['RECORDED'] === '' ? "N/A" : $meta['RECORDED'] );
+	$epi				= 's' . $meta['SEASON'] . 'e' . $meta['EPISODE'];
+	$meta['RECORDED']	= ( $meta['RECORDED'] === '' ? "N/A" : $meta['RECORDED'] );
 
 	echo "{$meta['DESCRIPTION']}<br>\n<br>\n";
 	echo "Shownotes featuring full descriptions and links can be found at https://steamlug.org/cast/{$epi}<br>\n";

commit 5b9e7f76cccb8448c9216bd734df962dbba1d464

Correct a typo in youtubethumb
Caused by a62be488413e506bd8e4969349460b81c84a7c47, bad John!

youtubethumb.php
@@ -103,7 +103,7 @@ if ($season !== "00" && $episode !== "00" && file_exists($filename))
 		$alignment = array(0, -91, -191, -291, -391, -491);
 		$gamesBlockOffset = $alignment[$games];
 
-		$plural = count($castGuests) > 1 ? "s" : "";
+		$plural = count($meta['GUESTS']) > 1 ? "s" : "";
 		$gamesString = <<<GAMESINTRO
 			<text id="game-name" style="font-size:23px;">With Special Guest{$plural} and Developer{$plural} of</text>
 			<g transform="translate({$gamesBlockOffset},26)">