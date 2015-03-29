c88f8e2277435651808ed66291c5cf01155561d6
John Drinkwater <john@nextraweb.com>
2015-03-29 15:26:11 +0100

    Twitter, prevent null events being an issue too

--- twitter.php
+++ twitter.php
@@ -82,7 +82,7 @@ if ( isset( $_POST['delete'] ) and isset( $_POST['key'] ) ) {
 	// Tweet does not exist:
 	// Array ( [errors] => Array ( [0] => Array ( [code] => 144 [message] => No status found with that ID. ) ) )
 	// Tweet delete ok:
-	// Deleted 576171168509624320 and got Array ( [created_at] => Fri Mar 13 00:01:25 +0000 2015 [id] => 576171168509624320 [id_str] => 576171168509624320 [text] => Hey #Linux gamers! Join us for some Guns of Icarus Online fun! Everybody's welcome! http://t.co/xylp8KEetF [source] => TweetDeck [truncated] => [in_reply_to_status_id] => [in_reply_to_status_id_str] => [in_reply_to_user_id] => [in_reply_to_user_id_str] => [in_reply_to_screen_name] => [user] => Array ( [id] => 1282779350 [id_str] => 1282779350 [name] => SteamLUG [screen_name] => SteamLUG [location] => [profile_location] => [description] => The Steam Linux User Group! A multilingual community of Linux gamers which aims to be a fun, welcoming space for people of all backgrounds and aptitudes [url] => http://t.co/UV563TiKNB [entities] => Array ( [url] => Array ( [urls] => Array ( [0] => Array ( [url] => http://t.co/UV563TiKNB [expanded_url] => http://steamlug.org [display_url] => steamlug.org [indices] => Array ( [0] => 0 [1] => 22 ) ) ) ) [description] => Array ( [urls] => Array ( ) ) ) [protected] => [followers_count] => 338 [friends_count] => 5 [listed_count] => 23 [created_at] => Wed Mar 20 09:10:33 +0000 2013 [favourites_count] => 30 [utc_offset] => [time_zone] => [geo_enabled] => [verified] => [statuses_count] => 851 [lang] => en [contributors_enabled] => [is_translator] => [is_translation_enabled] => [profile_background_color] => C0DEED [profile_background_image_url] => http://abs.twimg.com/images/themes/theme1/bg.png [profile_background_image_url_https] => https://abs.twimg.com/images/themes/theme1/bg.png [profile_background_tile] => [profile_image_url] => http://pbs.twimg.com/profile_images/3420706844/0169c9632f67b7928a84e723fb460380_normal.png [profile_image_url_https] => https://pbs.twimg.com/profile_images/3420706844/0169c9632f67b7928a84e723fb460380_normal.png [profile_link_color] => 0084B4 [profile_sidebar_border_color] => C0DEED [profile_sidebar_fill_color] => DDEEF6 [profile_text_color] => 333333 [profile_use_background_image] => 1 [default_profile] => 1 [default_profile_image] => [following] => [follow_request_sent] => [notifications] => ) [geo] => [coordinates] => [place] => [contributors] => [retweet_count] => 2 [favorite_count] => 2 [entities] => Array ( [hashtags] => Array ( [0] => Array ( [text] => Linux [indices] => Array ( [0] => 4 [1] => 10 ) ) ) [symbols] => Array ( ) [user_mentions] => Array ( ) [urls] => Array ( [0] => Array ( [url] => http://t.co/xylp8KEetF [expanded_url] => http://steamcommunity.com/groups/steamlug#events/116304948101305169 [display_url] => steamcommunity.com/groups/steamlu… [indices] => Array ( [0] => 84 [1] => 106 ) ) ) ) [favorited] => [retweeted] => [possibly_sensitive] => [lang] => en ) 
+	// Deleted 576171168509624320 and got Array ( [created_at] => Fri Mar 13 00:01:25 +0000 2015 [id] => 576171168509624320 [id_str] => 576171168509624320 [text] => Hey #Linux gamers! Join us for some Guns of Icarus Online fun! Everybody's welcome! http://t.co/xylp8KEetF [source] => TweetDeck [truncated] => [in_reply_to_status_id] => [in_reply_to_status_id_str] => [in_reply_to_user_id] => [in_reply_to_user_id_str] => [in_reply_to_screen_name] => [user] => Array ( [id] => 1282779350 [id_str] => 1282779350 [name] => SteamLUG [screen_name] => SteamLUG [location] => [profile_location] => [description] => The Steam Linux User Group! A multilingual community of Linux gamers which aims to be a fun, welcoming space for people of all backgrounds and aptitudes [url] => http://t.co/UV563TiKNB [entities] => Array ( [url] => Array ( [urls] => Array ( [0] => Array ( [url] => http://t.co/UV563TiKNB [expanded_url] => http://steamlug.org [display_url] => steamlug.org [indices] => Array ( [0] => 0 [1] => 22 ) ) ) ) [description] => Array ( [urls] => Array ( ) ) ) [protected] => [followers_count] => 338 [friends_count] => 5 [listed_count] => 23 [created_at] => Wed Mar 20 09:10:33 +0000 2013 [favourites_count] => 30 [utc_offset] => [time_zone] => [geo_enabled] => [verified] => [statuses_count] => 851 [lang] => en [contributors_enabled] => [is_translator] => [is_translation_enabled] => [profile_background_color] => C0DEED [profile_background_image_url] => http://abs.twimg.com/images/themes/theme1/bg.png [profile_background_image_url_https] => https://abs.twimg.com/images/themes/theme1/bg.png [profile_background_tile] => [profile_image_url] => http://pbs.twimg.com/profile_images/3420706844/0169c9632f67b7928a84e723fb460380_normal.png [profile_image_url_https] => https://pbs.twimg.com/profile_images/3420706844/0169c9632f67b7928a84e723fb460380_normal.png [profile_link_color] => 0084B4 [profile_sidebar_border_color] => C0DEED [profile_sidebar_fill_color] => DDEEF6 [profile_text_color] => 333333 [profile_use_background_image] => 1 [default_profile] => 1 [default_profile_image] => [following] => [follow_request_sent] => [notifications] => ) [geo] => [coordinates] => [place] => [contributors] => [retweet_count] => 2 [favorite_count] => 2 [entities] => Array ( [hashtags] => Array ( [0] => Array ( [text] => Linux [indices] => Array ( [0] => 4 [1] => 10 ) ) ) [symbols] => Array ( ) [user_mentions] => Array ( ) [urls] => Array ( [0] => Array ( [url] => http://t.co/xylp8KEetF [expanded_url] => http://steamcommunity.com/groups/steamlug#events/116304948101305169 [display_url] => steamcommunity.com/groups/steamlu… [indices] => Array ( [0] => 84 [1] => 106 ) ) ) ) [favorited] => [retweeted] => [possibly_sensitive] => [lang] => en )
 
 }
 
@@ -104,13 +104,20 @@ ACTIONMSG;
 }
 
 print "<!--\n";
-print_r ( $nextGameEvent );
-$eventDate = new DateTime(); $eventDate->setTimestamp($nextGameEvent['utctime']);
-$diff = date_diff($eventDate, new DateTime("now"));
-$difference = $diff->format("%h hours");
-$laterMessage = 'Hey #Linux gamers, join us for some ' . $nextGameEvent['title'] . " in {$difference}! Everybody’s welcome " . $nextGameEvent['url'];
-$typicalMessage = 'Hey #Linux gamers, join us for some ' . $nextGameEvent['title'] . ' fun! Everybody’s welcome ' . $nextGameEvent['url'];
-$when = str_replace( 'T', ' ', str_replace( '+00:00', '', date("c", $nextGameEvent['utctime'] ) ) );
+if ( $nextGameEvent != null ) {
+
+	// TODO check current time vs now; this is heavily reliant on XML information
+	print_r ( $nextGameEvent );
+	$eventDate = new DateTime(); $eventDate->setTimestamp($nextGameEvent['utctime']);
+	$diff = date_diff($eventDate, new DateTime("now"));
+	$difference = $diff->format("%h hours");
+	$laterMessage = 'Hey #Linux gamers, join us for some ' . $nextGameEvent['title'] . " in {$difference}! Everybody’s welcome " . $nextGameEvent['url'];
+	$typicalMessage = 'Hey #Linux gamers, join us for some ' . $nextGameEvent['title'] . ' fun! Everybody’s welcome ' . $nextGameEvent['url'];
+	$when = str_replace( 'T', ' ', str_replace( '+00:00', '', date("c", $nextGameEvent['utctime'] ) ) ) . '.';
+} else {
+	$when = 'a future date when someone creates the event!';
+	$laterMessage = $typicalMessage = "Hey #Linux gamers, join us for some gaming fun! Everybody’s welcome";
+}
 print "-->\n";
 ?>
 			<article class="panel panel-default twit">
@@ -118,7 +125,7 @@ print "-->\n";
 					<h3 class="panel-title">Event, gaming!</h3>
 				</header>
 				<div class="panel-body">
-					<p>This takes place on <?=$when;?>.</p>
+					<p>This takes place on <?=$when;?></p>
 					<form method="post" class="form-horizontal" action="/twitter/">
 						<fieldset>
 						<input type="hidden" name="tweet">
@@ -142,10 +149,11 @@ if ( $nextCastEvent != null ) {
 	print_r ( $nextCastEvent );
 	$eventDate = new DateTime(); $eventDate->setTimestamp($nextCastEvent['utctime']);
 	$diff = date_diff($eventDate, new DateTime("now"));
+	// TODO for ‘1 hours’, replace with ‘in an hour’. for < 2 days, turn days into hours and increment
 	$difference = $diff->format("%h hours");
 	$laterMessage = "Join us for the live recording of SteamLUG Cast in {$difference}, where we will be talking about %stuff. " . $nextCastEvent['url'];
 	$typicalMessage = "Join us for the live recording of SteamLUG Cast, where we will be talking about %stuff. " . $nextCastEvent['url'];
-	$when = str_replace( 'T', ' ', str_replace( '+00:00', '', date("c", $nextCastEvent['utctime'] ) ) );
+	$when = str_replace( 'T', ' ', str_replace( '+00:00', '', date("c", $nextCastEvent['utctime'] ) ) ) . '.';
 } else {
 	$when = 'a future date when someone creates the event!';
 	$laterMessage = $typicalMessage = "Join us for the live recording of SteamLUG Cast";
@@ -157,7 +165,7 @@ print "-->\n";
 					<h3 class="panel-title">Cast, recording</h3>
 				</header>
 				<div class="panel-body">
-					<p>This takes place on <?=$when;?>.</p>
+					<p>This takes place on <?=$when;?></p>
 					<form method="post" class="form-horizontal" action="/twitter/">
 						<fieldset>
 						<input type="hidden" name="tweet">
