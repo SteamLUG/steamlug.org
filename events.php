SteamLUG/steamlug.org

commit caa2de79ed288570719efc07529cd15fc37bf8a7

… and make all our calls to header as early as we can
But for scripts that need login_check() before output, then they include
session.php as early as possible.

events.php
@@ -1,5 +1,6 @@
 <?php
 $pageTitle = "Events";
+include_once( 'includes/header.php' );
 include_once( 'includes/functions_events.php' );
 
 $event = getNextEvent( );
@@ -11,7 +12,6 @@ if ($event != null) {
 }
 
 $externalJS = array('/scripts/events.js');
-include_once( 'includes/header.php' );
 ?>
 		<h1 class="text-center">SteamLUG Events</h1>
 		<article class="panel panel-default">

giveaway.php
@@ -1,4 +1,8 @@
-<?php include_once('includes/header.php'); ?>
+<?php
+$pageTitle = "Giveaways";
+
+include_once('includes/header.php');
+?>
         <header>
                 <h1>Giveaway</h1>
         </header>

includes/header.php
@@ -1,4 +1,6 @@
 <?php
+include_once('session.php');
+
 if (!isset($description))
 {
 	$description = "SteamLUG - the Steam Linux User Group! A multilingual community of Linux gamers which aims to be a fun, welcoming space for people of all backgrounds and aptitudes.";
@@ -13,7 +15,6 @@ if (!isset($pageTitle))
 	$pageTitle = "Super Secret Unnamed Page!";
 }
 
-include_once('session.php');
 $weareadmin = false;
 if(!login_check())
 {

projects.php
@@ -1,9 +1,8 @@
 <?php
-
 $pageTitle = "Projects";
 // TODO Clan pages?
+include_once('includes/header.php');
 ?>
-<?php include_once('includes/header.php'); ?>
 <h1 class="text-center">Projects - Coming Soon…</h1>
 <article class="panel panel-default">
 	<header class="panel-heading">

commit 23e9163d361972590eada689cfc08b11c10eb1f2

Fix issue with our timer caused by commit caa2de7

events.php
@@ -1,5 +1,6 @@
 <?php
 $pageTitle = "Events";
+$externalJS = array('/scripts/events.js');
 include_once( 'includes/header.php' );
 include_once( 'includes/functions_events.php' );
 
@@ -11,7 +12,6 @@ if ($event != null) {
 		$extraJS = "\t\t\tvar target = new Date(" . $eTime . ");";
 }
 
-$externalJS = array('/scripts/events.js');
 ?>
 		<h1 class="text-center">SteamLUG Events</h1>
 		<article class="panel panel-default">