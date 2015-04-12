3c039c74f7566f03fcf88de7113355d05b742672
John Drinkwater <john@nextraweb.com>
2015-04-12 20:48:38 +0100

    geturl, set timeouts to something reasonable
    This should prevent the behaviour we sometimes see with Twitch failing
    to send the streamers list.

--- includes/functions_geturl.php
+++ includes/functions_geturl.php
@@ -2,6 +2,9 @@
 
 if ( extension_loaded('curl') ) {
 
+	/* Our simple replacement for file_get_contents, with added bonus of param & header config
+	* If it fails, it will return a number. Currently naïvely tests for only a few errors.
+	*/
 	function geturl( $url, $get = array(), $header = array() ) {
 
 		$curl = curl_init( );
@@ -10,22 +13,22 @@ if ( extension_loaded('curl') ) {
 				CURLOPT_HTTPHEADER => $header,
 				CURLOPT_HEADER => 0,
 				CURLOPT_RETURNTRANSFER => 1,
-				CURLOPT_CONNECTTIMEOUT => 1,
-				CURLOPT_TIMEOUT => 1 )
+				CURLOPT_CONNECTTIMEOUT => 6,
+				CURLOPT_TIMEOUT => 8 )
 				);
 		$result = curl_exec( $curl );
 		$status = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
 
 		if ( ( $status == 404 ) || ( $status == 0 ) || ( $status == 503 ) ) {
-			return curl_error( $curl ) . ", " .curl_errno( $curl );
+			return $status;
 		}
 		curl_close( $curl );
 		return $result;
 	}
 } else {
-	
-	// should this ever work if curl does not exist? Unsure. Security.
-	function geturl( $url, $get = array(), $header = array() ) {
-		return false;
+
+	/* Curl isn’t on the server. */
+	function geturl( $url, $get, $header ) {
+		return 0;
 	}
 }
