312cd8261ab5f604775662772e73ed49d2ff410f
John Drinkwater <john@nextraweb.com>
2015-05-11 09:37:40 +0100

    Make our database schema use utf8mb4
    Because we want to support those annoying users called ‘johndrinkwater
    [🐧︀ 🎮]’. A great reference for migrating existing databases is at
    https://mathiasbynens.be/notes/mysql-utf8mb4 It also contains changes
    for my.cnf, and the calls needed in the client connection that our site
    will need to use: SET NAMES utf8mb4;

--- database.sql
+++ database.sql
@@ -15,12 +15,11 @@ DROP TABLE IF EXISTS `coteriebourgeois`;
 
 CREATE TABLE `apps` (
   `appid` int(11) unsigned NOT NULL,
-  `name` varchar(256) NOT NULL,
+  `name` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
   `onlinux` bit(1) NOT NULL DEFAULT b'0' COMMENT 'from SteamDB Linux List import… somewhere… stats update?',
-  `img_icon` varchar(140) DEFAULT '' COMMENT 'from events parser for now…',
-  PRIMARY KEY (`appid`),
-  UNIQUE KEY `appid_key` (`appid`)
-) DEFAULT CHARSET=utf8;
+  `img_icon` varchar(140) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'from events parser for now…',
+  PRIMARY KEY (`appid`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `appstats` (
   `date` date NOT NULL,
@@ -30,79 +29,78 @@ CREATE TABLE `appstats` (
   `fortnight` int(11) unsigned DEFAULT '0',
   PRIMARY KEY (`date`,`appid`),
   UNIQUE KEY `pairing` (`date`,`appid`)
-) DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `badges` (
   `badgeid` int(8) unsigned NOT NULL,
   PRIMARY KEY (`badgeid`),
-  UNIQUE KEY `badgeid_key` (`badgeid`)
-) DEFAULT CHARSET=utf8;
+  UNIQUE KEY `badgeid_UNIQUE` (`badgeid`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `clans` (
   `clanid` int(8) unsigned NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `creator` bigint(20) unsigned DEFAULT NULL,
   PRIMARY KEY (`clanid`),
-  UNIQUE KEY `clanid_key` (`clanid`)
-) DEFAULT CHARSET=utf8;
+  UNIQUE KEY `clanid_UNIQUE` (`clanid`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `eventattendance` (
-  `eventid` bigint(20) unsigned NOT NULL,
-  `steamid` bigint(20) unsigned NOT NULL,
+  `eventid` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `steamid` bigint(20) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`eventid`,`steamid`),
   UNIQUE KEY `pairing` (`eventid`,`steamid`)
-) DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `events` (
   `eventid` bigint(20) unsigned NOT NULL,
   `appid` int(11) unsigned NOT NULL,
-  `title` varchar(200) DEFAULT '',
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT '',
   `utctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `description` varchar(500) DEFAULT '' COMMENT 'copy/paste from steam :-\\',
+  `description` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'copy/paste from steam :-\\',
   `host` bigint(20) DEFAULT '0' COMMENT 'Should be the steamlug member ‘hosting’ the event; can be empty; set via backend',
-  `server` varchar(75) DEFAULT '' COMMENT 'Server deets from the event description; so event queries can work?; set via backend',
-  `serverport` varchar(5) DEFAULT '' COMMENT 'Server deets from the event description; so event queries can work?; set via backend',
+  `server` varchar(75) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'Server deets from the event description; so event queries can work?; set via backend',
+  `serverport` varchar(5) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'Server deets from the event description; so event queries can work?; set via backend',
   PRIMARY KEY (`eventid`,`appid`,`utctime`),
-  UNIQUE KEY `eventid_key` (`eventid`)
-) DEFAULT CHARSET=utf8;
+  UNIQUE KEY `eventid_UNIQUE` (`eventid`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `happenings` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `when` timestamp NULL DEFAULT NULL,
-  `what` varchar(100) DEFAULT '',
+  `what` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `log` (`id`)
-) DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `memberbadges` (
   `steamid` bigint(20) unsigned NOT NULL,
-  `badgeid` int(8) DEFAULT NULL COMMENT 'references badges table, should probably be a rng uuid rather than int',
+  `badgeid` int(8) DEFAULT NULL,
   `when` timestamp NULL DEFAULT NULL,
-  `trigger` varchar(45) DEFAULT NULL COMMENT 'describes what action caused badge (internal notekeeping?)',
+  `trigger` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'describes what action caused badge (internal notekeeping?)',
   PRIMARY KEY (`steamid`),
   UNIQUE KEY `pairing` (`steamid`,`badgeid`)
-) DEFAULT CHARSET=utf8;
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-CREATE TABLE `coteriebourgeois` (
+CREATE TABLE `memberclans` (
   `clanid` int(8) unsigned NOT NULL,
   `steamid` bigint(20) unsigned NOT NULL,
   `role` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`clanid`,`steamid`),
   UNIQUE KEY `pairing` (`clanid`,`steamid`)
-) DEFAULT CHARSET=utf8;
-
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `members` (
   `steamid` bigint(20) unsigned NOT NULL,
   `createdprofile` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Reference info, useful for future badge work?',
-  `personaname` varchar(200) DEFAULT NULL,
-  `profileurl` varchar(45) NOT NULL DEFAULT '' COMMENT 'Trimmed from PlayerSummary, can’t be unique or not null due to many people not having set this (and therefore '''')',
-  `avatar` varchar(130) DEFAULT NULL COMMENT 'Trimmed from PlayerSummary, avatarfull',
+  `personaname` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `profileurl` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 'Trimmed from PlayerSummary, can’t be unique or not null due to many people not having set this (and therefore '''')',
+  `avatar` varchar(130) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT 'Trimmed from PlayerSummary, avatarfull',
   `isgroupmember` bit(1) NOT NULL DEFAULT b'0' COMMENT 'When user logs in, we check this. Keep this in sync? As we limit actions like poll voting with it.',
-  `suggestedvisibility` tinyint(4) DEFAULT NULL COMMENT 'Taken from PlayerSummary, to suggest better defaults?',
+  `suggestedvisibility` tinyint(4) DEFAULT 0 COMMENT 'Taken from PlayerSummary, to suggest better defaults?',
   PRIMARY KEY (`steamid`,`profileurl`),
-  UNIQUE KEY `steamid_key` (`steamid`)
-) DEFAULT CHARSET=utf8;
+  UNIQUE KEY `steamid_UNIQUE` (`steamid`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `memberstats` (
   `date` date NOT NULL,
@@ -110,5 +108,5 @@ CREATE TABLE `memberstats` (
   `min` int(11) DEFAULT NULL,
   `max` int(11) DEFAULT NULL,
   PRIMARY KEY (`date`),
-  UNIQUE KEY `date_key` (`date`)
-) DEFAULT CHARSET=utf8;
+  UNIQUE KEY `date` (`date`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
