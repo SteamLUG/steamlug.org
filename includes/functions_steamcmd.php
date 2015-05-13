afb158ce469498c17dde2dce92fd4e28bd378554
John Drinkwater <john@nextraweb.com>
2015-05-13 19:37:27 +0100

    Add a function file for our steamcmd usage
    Related to the work for #169!

---
+++ includes/functions_steamcmd.php
@@ -0,0 +1,15 @@
+<?php
+	include_once('paths.php');
+
+	/* Small utility function, add creative description
+		Fetch and return icon from steamcmd
+		Unsure on the overlap between this and functions_steam
+	*/
+	function getAppIcon( $appid ) {
+
+		global $steamcmdPath;
+
+
+		// this returns the hash ‘e3f595a92552da3d664ad00277fad2107345f743’ for appid 440
+		return $icon;
+	}
