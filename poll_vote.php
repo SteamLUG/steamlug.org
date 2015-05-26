11015bc4b664ad872ddc89581e73e2299ac6bb44
John Drinkwater <john@nextraweb.com>
2015-05-26 17:21:19 +0100

    Update poll_vote to match our other scripts in layout, formatting

--- poll_vote.php
+++ poll_vote.php
@@ -1,104 +1,74 @@
 <?php
-	global $conn;
+$pageTitle = "Poll Vote";
+include_once('includes/session.php');
+include_once('includes/functions_poll.php');
 
-	include_once('includes/session.php');
-	include_once('includes/functions_db.php');
-	include_once('includes/functions_poll.php');
+if ( !array_key_exists( 'page', $_POST ) )
+	$_POST['page'] = "/";
 
-if ( !isset( $conn ) )
-	$conn = connectDB( );
+// are we logged in? no → leave
+if ( !login_check() ) {
+	header( "Location: " . $_POST['page'] );
+	exit();
+} else {
+	$me = $_SESSION['u'];
+}
 
-	if(login_check())
-	{
-		if (isset($_POST['poll']) && isset($_POST['poll_selection']))
+if ( !isset( $database ) )
+	$database = connectDB( );
+
+if (isset($_POST['poll']) && isset($_POST['poll_selection'])) {
+
+	if (is_numeric($_POST['poll']) && is_array($_POST['poll_selection'])) {
+
+		$stmt =  $database->prepare("select count(*) as voted from poll_respondent where uid = :uid");
+		$stmt->execute(array( 'uid' => $me ));
+		if ($stmt)
 		{
-			if (is_numeric($_POST['poll']) && is_array($_POST['poll_selection']))
-			{
-				//Check whether this uid has voted already
-				$stmt =  $conn->prepare("select count(*) as voted from poll_respondent where uid = :uid");
-				$stmt->execute(array( 'uid' => $uid ));
-				if ($stmt)
-				{
-					$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-					$stmt->closeCursor();
-					if ($result[0]['voted'] == 0)
-					{
+			$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$stmt->closeCursor();
+			if ($result[0]['voted'] == 0) {
 
-						$stmt =  $conn->prepare("select multipleChoice, now() between publishDate and expireDate as canVote from poll where id = :pollid");
-						$stmt->execute(array( 'pollid' => $_POST['poll']));
-						$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				$stmt =  $database->prepare("select multipleChoice, now() between publishDate and expireDate as canVote from poll where id = :pollid");
+				$stmt->execute(array( 'pollid' => $_POST['poll']));
+				$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				$stmt->closeCursor();
+				if ($result[0]['canVote'] == 1) {
+					//Check whether this is a multiple choice poll and bail if there are too many responses
+					if ((count($_POST['poll_selection']) > 1 && $result[0]['multipleChoice'] > 0) || (count($_POST['poll_selection']) == 1)) {
+
+						$stmt = $database->prepare("insert into poll_respondent (uid, pollID) values (:steamid, :pollid)");
+						$stmt->execute(array( 'steamid' => $me, 'pollid' => $_POST['poll']));
 						$stmt->closeCursor();
-						if ($result[0]['canVote'] == 1)
-						{
-							//Check whether this is a multiple choice poll and bail if there are too many responses
-							if ((count($_POST['poll_selection']) > 1 && $result[0]['multipleChoice'] > 0) || (count($_POST['poll_selection']) == 1))
-							{
-						
-								$stmt = $conn->prepare("insert into poll_respondent (uid, pollID) values (:steamid, :pollid)");
-								$stmt->execute(array( 'steamid' => $_SESSION['u'], 'pollid' => $_POST['poll']));
+
+						$stmt = $database->prepare("update poll_option set responseCount = responseCount + 1 where id = :optionid");
+
+						foreach ($_POST['poll_selection'] as $o) {
+							if ( is_numeric( $o ) ) {
+								$stmt->execute( array( 'optionid' => $o ) );
 								$stmt->closeCursor();
-
-								$stmt = $conn->prepare("update poll_option set responseCount = responseCount + 1 where id = :optionid");
-
-								foreach ($_POST['poll_selection'] as $o)
-								{
-									if (is_numeric($o))
-									{
-										$stmt->execute(array( 'optionid' => $o));
-										$stmt->closeCursor();
-									}
-								}
-							}
-							else
-							{
-								$error = "too_many_choices";
 							}
 						}
-						else
-						{
-							$error = "poll_not_open";
-						}
-
-
+					} else {
+						$error = "too_many_choices";
 					}
-					else
-					{
-						$error = "already_voted_" . $result[0]['voted'];
-					}
+				} else {
+					$error = "poll_not_open";
 				}
-				else
-				{
-					$error = "system_error";
-				}
-
+			} else {
+				$error = "already_voted_" . $result[ 0 ] ['voted' ];
 			}
-			else
-			{
-				$error = "bad_selection";
-			}
+		} else {
+			$error = "system_error";
 		}
-		else
-		{
-			$error = "bad_poll";
-		}
+	} else {
+		$error = "bad_selection";
 	}
+} else {
+	$error = "bad_poll";
+}
+
+if ( isset( $error ) )
+		header( 'location: ' . $_POST[ 'page' ] . '?error=' . $error );
 	else
-	{
-		$error = "logged_out";
-	}
-
-	if (!isset($_POST['page']))
-	{
-		$_POST['page'] = "/";
-	}
-	if (isset($error))
-	{
-		header("location: " . $_POST['page'] . "?error=" . $error);
-	}
-	else
-	{
-		header("location: " . $_POST['page']);
-	}
-
-
-?>
+		header( 'Location: ' . $_POST[ 'page' ] );
