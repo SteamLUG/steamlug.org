5902ade2c00503a5490fe4235d97bdff6a067568
Corben Dallas <corben.dallas@gmx.at>
2015-08-07 20:37:03 +0200

    Added favicon and apple-touch-icon using our google+ stencil tux icon from Cheeseness

--- includes/header.php
+++ includes/header.php
@@ -79,6 +79,30 @@ header("Cache-Control: public, max-age=60");
 		<script type="text/javascript">
 			var serverTime = <?= microtime(true); ?>;
 		</script>
+
+		<link rel="shortcut icon" type="image/x-icon" href="/_icons/favicon.ico" />
+		<link rel="icon" type="image/x-icon" href="/_icons/favicon.ico" />
+		<link rel="icon" type="image/gif" href="/_icons/favicon.gif" />
+		<link rel="icon" type="image/png" href="/_icons/favicon.png" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon.png" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-57x57.png" sizes="57x57" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-60x60.png" sizes="60x60" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-72x72.png" sizes="72x72" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-76x76.png" sizes="76x76" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-114x114.png" sizes="114x114" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-120x120.png" sizes="120x120" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-128x128.png" sizes="128x128" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-144x144.png" sizes="144x144" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-152x152.png" sizes="152x152" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-180x180.png" sizes="180x180" />
+		<link rel="apple-touch-icon" href="/_icons/apple-touch-icon-precomposed.png" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-160x160.png" sizes="160x160" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-192x192.png" sizes="192x192" />
+		<link rel="icon" type="image/png" href="/_icons/favicon-196x196.png" sizes="196x196" />
+
 <?php
 
 	if (isset($extraCrap))
