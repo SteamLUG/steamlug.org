SteamLUG/steamlug.org

commit 9ed144d9326354f6da60cc90170bdeb5c7ecf3aa

Avatars, add support for resizing and converting to PNG by default
Tested with direct file upload, haven’t tried with the other options
but they should functionally work the same.
This needs better error handling, mostly because it doesn’t return
information yet for if the conversion failed - and we need to make sure
a failure there doesn’t prevent further image uploads.

avatar.php
@@ -32,9 +32,13 @@ if ( isset( $_GET['name'] ) and isset( $_GET['key'] ) ) {
 	} else { $continue = false; }
 
 	if ( $continue and ( $givenKey === $requestedKey ) ) {
+		$originalPath = $avatarFilePath . '/original/' . $requestedName . '.png';
 		$requestedPath = $avatarFilePath . '/' . $requestedName . '.png';
 		$requestedURL  = $_SESSION['a']; // we trust Valve gave us unshit URL
-		$result = writeURLToLocation( $requestedURL, $requestedPath, false );
+		$result = writeURLToLocation( $requestedURL, $originalPath, false );
+		if ( $result ) {
+			$result = resizeAvatar( $originalPath, $requestedPath );
+		}
 	} else { $continue = false; }
 
 	if ( $continue and $result ) {
@@ -61,12 +65,13 @@ if ( isset( $_POST['name'] ) and isset( $_FILES['userfile'] ) ) {
 
 	$action = "Upload File";
 	$requestedName = sanitiseName( $_POST['name'] );
+	$originalPath = $avatarFilePath . '/original/' . $requestedName . '.png';
 	$requestedPath = $avatarFilePath . '/' . $requestedName . '.png';
 	$hostedURL		= '/avatars/' . $requestedName . '.png';
 	$override		= ( array_key_exists( 'overwrite', $_POST ) ? $_POST['overwrite'] : false );
 
 	// do we want to be able to overwrite?
-	if ( (!file_exists( $requestedPath ) or $override == true) and !is_dir( $requestedPath ) ) {
+	if ( (!file_exists( $originalPath ) or $override == true) and !is_dir( $originalPath ) ) {
 
 		if ( is_uploaded_file( $_FILES['userfile']['tmp_name'] ) and ( $_FILES['userfile']['size'] < 500000 ) ) {
 
@@ -76,11 +81,15 @@ if ( isset( $_POST['name'] ) and isset( $_FILES['userfile'] ) ) {
 
 			if ( ( $image !== false and in_array( $image[2],  $typesAllowed ) ) ) {
 
-				if ( move_uploaded_file($_FILES['userfile']['tmp_name'], $requestedPath ) ) {
+				// TODO overwrite issues for requestPath?
+				if ( move_uploaded_file($_FILES['userfile']['tmp_name'], $originalPath ) ) {
 
+					$result = resizeAvatar( $originalPath, $requestedPath );
 					// success
-					writeAvatarLog( 0, $me, $requestedName, 'upload' );
-					$body = "<p>File uploaded. [<img height=\"14\" width=\"14\" src=\"{$hostedURL}\" />]</p>";
+					if ( $result ) {
+						writeAvatarLog( 0, $me, $requestedName, 'upload' );
+						$body = "<p>File uploaded. [<img height=\"14\" width=\"14\" src=\"{$hostedURL}\" />]</p>";
+					}
 				} else {
 
 					$style = "panel-danger";
@@ -159,13 +168,19 @@ if ( isset( $_GET['email'] ) and isset( $_GET['name'] ) ) {
 	$requestedURL	= "http://www.gravatar.com/avatar/" . $gravatar;
 	$hostedURL		= '/avatars/' . $requestedName . '.png';
 
+	$originalPath = $avatarFilePath . '/original/' . $requestedName . '.png';
 	$requestedPath = $avatarFilePath . '/' . $requestedName . '.png';
 	/* this returns false for existing file */
-	$result = writeURLToLocation( $requestedURL, $requestedPath, false );
+	$result = writeURLToLocation( $requestedURL, $originalPath, false );
 
 	if ( $result ) {
-		writeAvatarLog( 0, $me, $requestedName, 'gravatar' );
-		$body = "<p>Fetched gravatar for user {$requestedName}. [<img height=\"14\" width=\"14\" src=\"{$requestedURL}\" />] [<img height=\"14\" width=\"14\" src=\"{$hostedURL}\" />]</p><p>These images should match.</p>";
+
+		$result = resizeAvatar( $originalPath, $requestedPath );
+		if ( $result ) {
+
+			writeAvatarLog( 0, $me, $requestedName, 'gravatar' );
+			$body = "<p>Fetched gravatar for user {$requestedName}. [<img height=\"14\" width=\"14\" src=\"{$requestedURL}\" />] [<img height=\"14\" width=\"14\" src=\"{$hostedURL}\" />]</p><p>These images should match.</p>";
+		} /* TODO: error for resize. also, change this to failure waterfall */
 	} else {
 		$style = "panel-danger";
 		$body = "<p>Failed fetching gravatar for user {$requestedName}, confirm email is attached to their system and we don’t have that user already.</p>";

includes/functions_avatars.php
@@ -100,8 +100,28 @@
 		return $avatars;
 	}
 
-	/* TODO make a function here to crop/resize avatar and output as a PNG
-	producing a local backup of the source too */
+	/* read from a file of some type, write to a PNG file */
+	function resizeAvatar( $incoming, $outgoing, $overwrite = false ) {
+
+		if ( file_exists( $outgoing ) and !is_dir( $outgoing ) and ( !$overwrite ) ) {
+			return false;
+		}
+		if ( !file_exists( $incoming ) and !is_dir( $incoming ) ) {
+			return false;
+		}
+		
+		$commandresize = "convert -resize 256x256 -type optimize -strip {$incoming} png:{$outgoing}";
+		ob_start( );
+		echo shell_exec( $commandresize . ' 2>&1' );
+		$debugoutput = ob_get_clean();
+
+		if ( file_exists( $outgoing ) ) {
+			/* do anything here? */
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 if ( extension_loaded('curl') ) {
 

commit a4e4c96537d72a9b6c4c943d7ee833c0596acfc0

Remove both avatar files when admin triggers a delete
When we added resize support, we made the site generate a backup image,
and resize into a new file. Our deletion code did not know about this,
so when you go to upload a new file, it would fail because the file
already exists.

Fixes a bug introduced in 9ed144d932635

avatar.php
@@ -209,9 +209,15 @@ if ( isset( $_GET['delete'] ) and isset( $_GET['name'] ) ) {
 
 	$action = "Remove Avatar";
 	$requestedName = sanitiseName( $_GET['name'] );
+	$originalPath = $avatarFilePath . '/original/' . $requestedName . '.png';
 	$requestedPath = $avatarFilePath . '/' . $requestedName . '.png';
 
 	if ( file_exists( $requestedPath ) and !is_dir( $requestedPath ) ) {
+
+		if ( file_exists( $originalPath ) and !is_dir( $originalPath ) ) {
+			unlink( $originalPath );
+		}
+
 		writeAvatarLog( 0, $me, $requestedName, 'delete' );
 		$body = "<p>Removed avatar file for user {$requestedName}.</p>";
 		unlink( $requestedPath );