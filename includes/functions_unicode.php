e7e03e09cbda3242d4a1516da680c63213695709
John Drinkwater <john@nextraweb.com>
2015-11-17 20:29:01 +0000

    Add a method to get unicode regional indicator from CC
    This is currently held up on getting around Chrome rejecting/breaking
    the display of colour font support.
    
    Intention is to retire flag images and use some more unicode goodness
    encoded in our site font.

---
+++ includes/functions_unicode.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* Function collection for … no good reason, just wanted a linkable function reference for:
+*/
+
+/**
+ * Converts a two letter ISO country code (from geoip_country_code_by_name) and emits Unicode regional indicators.
+ * @param string $country ISO 3166-1 alpha-2 country code, two characters long. If false, returns ''
+ * @return string Some lovely Unicode, that may be rendered as a flag, depending on platform support
+*/
+function country_code_to_unicode( $country ) {
+
+	if ( ( $country === false ) or ( strlen( $country ) != 2 ) )
+		return '';
+
+	$country = strtoupper( $country );
+
+	$char1 = ord( $country[0] ) - 65;
+	$char2 = ord( $country[1] ) - 65;
+
+	if (( $char1 > 25 ) or ( $char2 > 25 ) or
+		( $char1 <  0 ) or ( $char2 <  0 ) )
+		return '';
+
+	$start = 0x1F1E6;
+	$char1 += $start; $char2 += $start;
+	return mb_convert_encoding('&#' . $char1 . ';&#' . $char2 . ';', 'UTF-8', 'HTML-ENTITIES');
+}
